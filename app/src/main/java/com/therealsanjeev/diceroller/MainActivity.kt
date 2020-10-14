13481276ab94e1fdd7ebf87c09de5f0dbaa0e7a7
Prateek <codes.prateek@gmail.com>
2020-10-14 19:26:14 +0530

    MainActivity code
    I have added main activity code using Kotlin. Enjoy 👍

--- app/src/main/java/com/therealsanjeev/diceroller/MainActivity.kt
+++ app/src/main/java/com/therealsanjeev/diceroller/MainActivity.kt
@@ -4,8 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var diceImage: ImageView
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val diceButton: Button = findViewById(R.id.roll_button)
+        diceButton.setOnClickListener {
+            rollDice()
+            //Toast.makeText(this, "Hey, Roller Clicked ", Toast.LENGTH_SHORT).show()
+        }
+        diceImage = findViewById(R.id.dice_img)
+    }
+    
+    private fun rollDice() {
+        //val resultText: TextView = findViewById(R.id.dice_text)
+        // resultText.text = randomNum.toString()
+        val randomNum = Random.nextInt(6)+1
+        val drawableRes = when(randomNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableRes)
     }
 }
